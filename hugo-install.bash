76f795e7a1b217ea2dc8c98c4e2c0699b803b829
drruruu ☔ <hello@drruruu.dev>
2021-05-15 10:17:02 -0500

    Update hugo install

--- hugo-install.bash
+++ hugo-install.bash
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 # installs the hugo program
 
+set -o errexit
+set -o noclobber
+set -o noglob
+set -o nounset
+set -o physical
+set -o pipefail
 set -o xtrace
 
-    repo='gohugoio/hugo'
+repo='gohugoio/hugo'
+
 platform='Linux'
-    arch='64bit'
+
+arch='64bit'
 
 pcre="hugo_[\d]+\.[\d]+\.[\d]+_$platform-$arch.tar.gz"
 
@@ -18,8 +26,14 @@ fi
 cd /tmp
 
 gh auth refresh
-tarball="$(gh release view --repo "$repo"|grep --perl-regexp "$pcre"|cut -f2)"
-gh release download --repo "$repo" latest "$tarball"
-tar --extract --file "$tarball"
 
+tarball="$(gh release view --repo "$repo"|grep --perl-regexp "$pcre"|cut -f2)"
+
+if [[ -f "$tarball" ]] 
+then
+  rm "$tarball"
+fi
+gh release download --repo "$repo" --pattern "$tarball"
+
+tar --extract --file "$tarball"
 install hugo /usr/bin/ 
