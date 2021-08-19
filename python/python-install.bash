c229b25ba84d8adb6f232ffe2bf34489f63d79c6
dru <hello@drruruu.dev>
2021-08-19 09:50:06 -0500

    Update python-install

--- python/python-install.bash
+++ python/python-install.bash
@@ -11,29 +11,68 @@ set -o xtrace
 
 # SEE: https://github.com/python/cpython#build-instructions <dru 2020-08-19>
 
+py_version='3.9.6'
+py_method='wget' # 'git'
+py_dir='/tmp/python/'
+
+# CAVEAT: defines *additional* build dependencies <dru 2020-08-19>
+declare -a py_deps
+py_deps+=('build-essential')
+py_deps+=('gdb')
+py_deps+=('git')
+py_deps+=('lcov')
+py_deps+=('libbz2-dev')
+py_deps+=('libffi-dev')
+py_deps+=('libgdbm-dev')
+py_deps+=('liblzma-dev')
+py_deps+=('libncurses5-dev')
+py_deps+=('libreadline6-dev')
+py_deps+=('libsqlite3-dev')
+py_deps+=('libssl-dev')
+py_deps+=('lzma')
+py_deps+=('lzma-dev')
+py_deps+=('tk-dev')
+py_deps+=('uuid-dev')
+py_deps+=('zlib1g-dev')
+
+declare -a script_deps
+case "$py_method" in
+  'git')
+    script_deps+=('git') 
+    ;;
+  'wget')
+    script_deps+=('tar') 
+    script_deps+=('wget')
+    ;;
+   *)
+    exit 1
+    ;;
+esac
+
 sudo apt-get update
 
-# TODO: use git as recommended in dev guide <dru 2020-08-19>
-sudo apt-get install -y wget
+sudo apt-get build-dep 'python3'
 
-sudo apt-get build-dep python3
+sudo apt-get -y install "${py_deps[@]}" "${script_deps[@]}"
 
-sudo apt-get -y install build-essential gdb lcov libbz2-dev libffi-dev \
-      libgdbm-dev liblzma-dev libncurses5-dev libreadline6-dev \
-      libsqlite3-dev libssl-dev lzma lzma-dev tk-dev uuid-dev zlib1g-dev
+[[ -d "$py_dir" ]] && sudo rm -rf "$py_dir"
 
-cd /tmp
+case "$py_method" in
+  'git')
+    py_tag="v$py_version"
+    git clone --depth '1' --branch "$py_tag" 'https://github.com/python/cpython' "$py_dir"
+    ;;
+  'wget')
+    py_url="https://www.python.org/ftp/python/$py_version/Python-$py_version.tgz"
+    mkdir "$py_dir"
+    wget -qO- "$py_url" | tar xvz -C "$py_dir" --strip-components '1'
+    ;;
+   *)
+    exit 1
+    ;;
+esac
 
-url='https://www.python.org/ftp/python/3.9.6/Python-3.9.6.tgz'
-tgz="${url##*/}"
-dir="${tgz%.*}"
-
-wget -O "$tgz" "$url"
-tar xf "$tgz"
-
-cd "$dir"
-# HACK: make fails without debug flag <dru 2020-08-19>
-./configure --with-pydebug
+cd "$py_dir"
+./configure
 make -j2
-# make test
 sudo make install
