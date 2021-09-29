08879adeaf79d504a116751b05d27d52a7c87f35
dru <hello@drruruu.dev>
2021-09-29 00:31:25 -0500

    Update docker install

--- docker/docker-install.bash
+++ docker/docker-install.bash
@@ -1,35 +1,55 @@
 #!/usr/bin/env bash
 
-# Installs the docker apt repository and docker
+# Installs the docker apt repository and docker.
 
-# REQ: https://docs.docker.com/engine/install/debian/ <dru 2020-06-14>
+# REQ: https://docs.docker.com/engine/install/debian/ <>
+# REQ: https://docs.docker.com/engine/install/linux-postinstall/ <>
 
+# HACK: update-alternatives --config iptables for WSL2. <dru 2021-09-28>
+
+set +o braceexpand
 set -o errexit
 set -o noclobber
 set -o noglob
 set -o nounset
 set -o pipefail
-set -o xtrace
 
-keyserver=https://download.docker.com/linux/debian/gpg
-keyring=/usr/share/keyrings/docker-archive-keyring.gpg
-fingerprint=8D81803C0EBFCD88
+# set -o verbose
+# set -o xtrace
 
-list=docker.list
+keyserver='https://download.docker.com/linux/debian/gpg'
+keyring='/usr/share/keyrings/docker-archive-keyring.gpg'
+fingerprint='8D81803C0EBFCD88'
+list='/etc/apt/sources.list.d/docker.list'
 
-declare -a deps
-deps+=(apt-transport-https)
-deps+=(ca-certificates)
-deps+=(curl)
-deps+=(gnupg)
-deps+=(lsb-release)
-deps+=(software-properties-common)
+declare -ar dependencies=(
+  'apt-transport-https'
+  'ca-certificates'
+  'curl'
+  'gnupg'
+  'lsb-release'
+)
+
+declare -a packages=(
+    'docker-ce'
+    'docker-ce-cli'
+    'containerd.io'
+)
 
 sudo apt-get update
-sudo apt-get install ${deps[@]}
+sudo apt-get install "${dependencies[@]}"
 
-gpg --no-default-keyring --keyring $keyring --keyserver $keyserver --recv-keys $fingerprint
-sudo add-apt-repository "deb [arch=amd64 signed-by=$ring] $url $(lsb_release -cs) stable" $list
+sudo gpg \
+  --no-default-keyring \
+  --keyring "$keyring" \
+  --keyserver "$keyserver" \
+  --recv-keys "$fingerprint"
+
+sudo bash -c "echo 'deb [arch=amd64 signed-by=$keyring] ${keyserver%/*} $(lsb_release -cs) stable' > $list"
 
 sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io
+sudo apt install "${packages[@]}"
+
+sudo groupadd docker
+sudo usermod -aG docker "$USER"
+newgrp docker
