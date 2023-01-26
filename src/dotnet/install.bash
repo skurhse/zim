4657e272f6cfe1573d53e84f16e585683e3c3ac6
Skurhse Rage 🌆🌃🌌 <skurhse@rage.codes>
2023-01-26 02:44:03 -0600

    dotnet

--- src/dotnet/install.bash
+++ src/dotnet/install.bash
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-# installs the dotnet sdk. <skr 2023-01-25>
+# installs the dotnet sdk with apt. <skr 2023-01-25>
 
-# https://github.com/dotnet/core/blob/main/release-notes/6.0/preview/6.0.0-preview.4-install-instructions.md
+# https://learn.microsoft.com/en-us/dotnet/core/install/linux-debian <>
 
 set +o braceexpand
 set -o errexit
@@ -12,7 +12,7 @@ set -o nounset
 set -o pipefail
 set -o xtrace
 
-repo=dotnet
+repo=microsoft-debian-bullseye-prod
 package=dotnet-sdk-7.0
 
 keyserver=https://packages.microsoft.com/keys/microsoft.asc
@@ -21,10 +21,7 @@ fingerprint=0xEB3E94ADBE1229CF
 
 url=https://packages.microsoft.com/repos/$repo/
 arch=amd64
-list=/etc/apt/sources.list.d/$repo.list
-
-sudo apt-get update
-sudo apt-get install --assume-yes curl gpg lsb-release
+list=/etc/apt/sources.list.d/dotnet.list
 
 gpg --show-keys --keyid-format 0xLONG <(curl $keyserver)
 
@@ -38,11 +35,12 @@ sudo gpg --no-default-keyring          \
 	 --keyid-format       0xLONG   \
 	 --list-keys
 
-str="deb [arch=$arch signed-by=$keyring] $repo $(lsb_release -cs) main"
+str="deb [arch=$arch signed-by=$keyring] $url $(lsb_release -cs) main"
 
 sudo bash -c "echo ${str@Q} > $list"
 cat $list
 
 sudo apt-get update
 sudo apt-get --assume-yes install $package
-$package --version
+
+dotnet --version
