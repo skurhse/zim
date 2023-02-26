181a487bf8bf2864ade4d74f7c207c53da845cd8
Skurhse Rage 🌆🌃🌌 <skurhse@rage.codes>
2023-02-25 22:26:17 -0600

    vscode-langserver

---
+++ src/neovim/vscode-langserver.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# REQ: Installs the Visual Studio Code language servers with npm for use with neovim's eslint lspconfig. <skr 2023-02-25>
+
+# SEE: https://github.com/hrsh7th/vscode-langservers-extracted <>
+# SEE: https://eslint.org/docs/latest/ <>
+# SEE: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#eslint <>
+
+set +o braceexpand
+set -o errexit
+set -o noclobber
+set -o nounset
+set -o noglob
+set -o pipefail
+set -o xtrace
+
+npm --version
+npm i -g vscode-langservers-extracted
