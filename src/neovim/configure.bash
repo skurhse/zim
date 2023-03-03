e329af24b6a8c668ef61e60949c0b1b88eefd0b2
Skurhse Rage 🌆🌃🌌 <skurhse@rage.codes>
2023-03-02 23:14:32 -0600

    Update neovim configuration

--- src/neovim/configure.bash
+++ src/neovim/configure.bash
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-# REQ: Installs neovim's init.lua from Gist using the Github CLI. <skr 2023-02-25>
-
-# SEE: https://neovim.io/doc/user/lua-guide.html <>
-# SEE: https://neovim.io/doc/user/lua.html <>
+# REQ: Installs the neovim init configuration from Gist using the Github CLI. <skr 2023-03-02>
 
 set +o braceexpand
 set -o errexit
@@ -13,14 +10,21 @@ set -o nounset
 set -o pipefail
 set -o xtrace
 
-url=https://gist.github.com/skurhse/3071390d8a63efdd6e90542ac8c6115d
-file=init.lua
-dir=/tmp/${url##*/}/
-src=$dir/$file
-trg=~/.config/nvim/
+declare -A gist=(
+  ['id']='3071390d8a63efdd6e90542ac8c6115d'
+  ['user']='skurhse'
+  ['filename']='init.lua'
+)
+gist['url']="https://gist.github.com/${gist['user']}/${gist['id']}"
 
+tmp_dir='/tmp/${gist[url]##*/}/'
+source_file="${tmp_dir}/${gist['filename']}"
+target_dir=~/'.config/nvim/'
 
-rm -rf $dir
-gh gist clone $url $dir
-mkdir -p $trg
-cp $src $trg 
+rm --force --recursive --verbose "$tmp_dir"
+
+gh gist clone "${gist['url']}" "$tmp_dir" -- --verbose
+
+mkdir --parents --verbose "$target_dir"
+
+cp --verbose "$source_file" "$target_dir" 
