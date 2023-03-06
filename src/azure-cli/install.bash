6d8973a96d1b1bcbcb007fa3a969d6dcb9edaf38
Skurhse Rage 🌆🌃🌌 <skurhse@rage.codes>
2023-03-05 20:32:01 -0600

    Update azure-cli

--- src/azure-cli/install.bash
+++ src/azure-cli/install.bash
@@ -1,50 +1,56 @@
 #!/usr/bin/env bash
 
-# installs the azure cli with upgrades and extensions. <skr 2023-01-25>
+# Installs the latest Azure CLI. <skr 2023-03-05>
 
 # https://learn.microsoft.com/en-us/cli/azure/install-azure-cli-linux <>
 
-set +o braceexpand
-set -o errexit
-set -o noclobber
-set -o noglob
-set -o nounset
-set -o pipefail
-set -o xtrace
+set +o 'braceexpand'
+set -o 'errexit'
+set -o 'noclobber'
+set -o 'noglob'
+set -o 'nounset'
+set -o 'pipefail'
+set -o 'xtrace'
 
-keyserver=https://packages.microsoft.com/keys/microsoft.asc
-keyring=/usr/share/keyrings/microsoft.gpg
-fingerprint=0xEB3E94ADBE1229CF
-upgrades=(bicep)
-extensions=(aks-preview)
+declare -Ar dependencies=(
+  [curl]='7.74.0-1.3+deb11u7'
+  [gpg]='2.2.27-2+deb11u2'
+  [lsb-release]='11.1.0'
+)
 
-repo=https://packages.microsoft.com/repos/azure-cli/
-arch=amd64
-list=/etc/apt/sources.list.d/microsoft.list
+readonly keyserver='https://packages.microsoft.com/keys/microsoft.asc'
+readonly keyring='/usr/share/keyrings/microsoft.gpg'
+readonly fingerprint='0xEB3E94ADBE1229CF'
 
-sudo apt-get update
-sudo apt-get install --assume-yes curl gpg lsb-release
+readonly repo='https://packages.microsoft.com/repos/azure-cli/'
+readonly arch='amd64'
+readonly list='/etc/apt/sources.list.d/microsoft.list'
 
-gpg --show-keys --keyid-format 0xLONG <(curl $keyserver)
+for package in "${!dependencies[@]}"; do
+  actual="$(dpkg-query --show --showformat='${Version}' "$package")"
+  if [[ "$actual" != "${dependencies[$package]}" ]]; then
+    echo "Package $package is version $actual, expected ${dependencies[$package]}"
+    exit 1
+  fi
+done
 
-sudo gpg --no-default-keyring               \
-	 --keyring             $keyring     \
-	 --keyserver           $keyserver   \
-         --recv-keys           $fingerprint
+gpg --show-keys --keyid-format '0xLONG' <(curl "$keyserver")
 
-sudo gpg --no-default-keyring          \
-	 --keyring            $keyring \
-	 --keyid-format       0xLONG   \
-	 --list-keys
+sudo gpg --no-default-keyring \
+  --keyring   "$keyring" \
+  --keyserver "$keyserver" \
+  --recv-keys "$fingerprint"
 
-str="deb [arch=$arch signed-by=$keyring] $repo $(lsb_release -cs) main"
+sudo gpg --no-default-keyring \
+  --keyring      "$keyring" \
+  --keyid-format '0xLONG' \
+  --list-keys
+
+release="$(lsb_release -cs)"
+str="deb [arch=$arch signed-by=$keyring] $repo $release main"
 
 sudo bash -c "echo ${str@Q} > $list"
 cat $list
 
 sudo apt-get update
 sudo apt-get --assume-yes install azure-cli
-az --version
-
-for upg in ${upgrades[@]}; do az $upg upgrade; done
-for ext in ${extensions[@]}; do az extension add --upgrade --name $ext; done
