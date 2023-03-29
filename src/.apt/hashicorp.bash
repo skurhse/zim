70ad55ca928603d4e5edc69fa9b0860aadbd0dec
Skurhse Rage 🌆🌃🌌 <skurhse@rage.codes>
2023-03-29 04:53:41 -0500

    Add hashicorp repository

--- src/.apt/hashicorp.bash
+++ src/.apt/hashicorp.bash
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-# REQ: Installs the Microsoft Debian APT repository. <skr 2023-03-28>
+# REQ: Installs the Hashicorp APT repository. <skr 2023-03-28>
 
-# SEE: https://github.com/microsoft/linux-package-repositories <>
+# SEE: https://www.hashicorp.com/official-packaging-guide <>
 
-# PORT: Bookworm not yet supported. <skr 2023-03-27>
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 set +o braceexpand
 set -o errexit
@@ -17,21 +19,37 @@ set -o xtrace
 readonly dependencies=('curl' 'gpg')
 
 for package in "${dependencies[@]}"; do
-  dpkg-query --show "$package"
+  if status=$(dpkg-query --show --showformat '${db:Status-Status}' "$package"); then
+    if [[ "$status" != 'installed' ]]; then
+      echo "Unexpected status $status for package $package." >&2
+      exit 4
+    fi
+  else
+    if [[ $? -eq 1 ]]; then
+      echo "Package $package not found." >&2
+      echo "To install:" >&2
+      echo "  sudo apt-get install $package" >&2
+    else
+      echo "dpkg-query failed with unexpected status $status." >&2
+      exit 5
+    fi
+  fi
 done
 
 architecture=$(dpkg --print-architecture)
+distribution=$(lsb_release -cs)
 
 readonly architecture
-readonly keyring='/usr/share/keyrings/microsoft.gpg'
-readonly repository="microsoft-debian-bullseye-prod"
-readonly distribution='bullseye'
+readonly keyring='/usr/share/keyrings/hashicorp.gpg'
+readonly repository="hashicorp"
+readonly distribution
+
 readonly component='main'
 
-readonly keyserver='https://packages.microsoft.com/keys/microsoft.asc'
-readonly fingerprint='0xEB3E94ADBE1229CF'
+readonly keyserver='https://apt.releases.hashicorp.com/gpg'
+readonly fingerprint='798AEC654E5C15428C8E42EEAA16FCBCA621E701'
 
-readonly url="https://packages.microsoft.com/repos/$repository/"
+readonly url='https://apt.releases.hashicorp.com'
 readonly list="/etc/apt/sources.list.d/$repository.list"
 
 gpg --show-keys --keyid-format 0xLONG <(curl "$keyserver")
