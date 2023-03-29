02dfd45663cc57da239cc72ee5a58cbe39fc0482
Skurhse Rage 🌆🌃🌌 <skurhse@rage.codes>
2023-03-29 05:38:59 -0500

    Remove defunct

--- .unsorted/minikube/start.bash
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set +B -Cefuvxo pipefail
-
-sudo service docker start
-minikube start
--- .unsorted/minikube/stop.bash
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set +B -Cefuvxo pipefail
-
-minikube stop
-sudo service docker stop
