ee09fbfaf523d61c7025279d6bfbbb76b884e975
Skurhse Rage 🌆🌃🌌 <skurhse@rage.codes>
2023-03-29 05:23:36 -0500

    Remove dlv

--- .unsorted/dlv/install.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# REQ: Installs delve with `go install`. <skr 2022-01-15>
-# SEE: https://github.com/go-delve/delve/tree/master/Documentation/installation
-# ..............................................................................
-set +B -Cefuvxo pipefail
-
-go install github.com/go-delve/delve/cmd/dlv@latest
-
-dlv version
