cb9cb0214968312481afa6e2cb11352a2c8e3c55
Skurhse Rage 🌆🌃🌌 <skurhse@rage.codes>
2023-03-31 20:30:54 -0500

    Update hashicorp

--- apt/hashicorp.bash
+++ apt/hashicorp.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# REQ: Installs the Hashicorp APT repository. <skr 2023-03-28>
+# REQ: Installs the Hashicorp APT repository and signing key. <skr 2023-03-31>
 
 # SEE: https://www.hashicorp.com/official-packaging-guide <>
 
@@ -16,43 +16,43 @@ set -o nounset
 set -o pipefail
 set -o xtrace
 
-readonly dependencies=('curl' 'gpg')
+readonly dependencies=('curl' 'gpg' 'lsb-release')
 
 for package in "${dependencies[@]}"; do
   if status=$(dpkg-query --show --showformat '${db:Status-Status}' "$package"); then
     if [[ "$status" != 'installed' ]]; then
       echo "Unexpected status $status for package $package." >&2
-      exit 4
+      exit 3 # 
     fi
   else
     if [[ $? -eq 1 ]]; then
       echo "Package $package not found." >&2
       echo "To install:" >&2
       echo "  sudo apt-get install $package" >&2
+      exit 2
     else
-      echo "dpkg-query failed with unexpected status $status." >&2
-      exit 5
+      echo "dpkg-query failed with status $status." >&2
+      exit 1
     fi
   fi
 done
 
 architecture=$(dpkg --print-architecture)
-distribution=$(lsb_release -cs)
+distribution=$(lsb_release --short --codename)
 
 readonly architecture
 readonly keyring='/usr/share/keyrings/hashicorp.gpg'
 readonly repository="hashicorp"
 readonly distribution
-
 readonly component='main'
 
 readonly keyserver='https://apt.releases.hashicorp.com/gpg'
 readonly fingerprint='798AEC654E5C15428C8E42EEAA16FCBCA621E701'
 
 readonly url='https://apt.releases.hashicorp.com'
-readonly list="/etc/apt/sources.list.d/$repository.list"
+readonly list='/etc/apt/sources.list.d/hashicorp.list'
 
-gpg --show-keys --keyid-format 0xLONG <(curl "$keyserver")
+gpg --show-keys <(curl "$keyserver")
 
 sudo gpg \
   --no-default-keyring       \
@@ -63,7 +63,6 @@ sudo gpg \
 sudo gpg \
   --no-default-keyring      \
   --keyring      "$keyring" \
-  --keyid-format 0xLONG     \
   --list-keys
 
 readonly source="deb [arch=$architecture signed-by=$keyring] $url $distribution main"
