e430c008c929e27628e241fe2c21743b9bb632fa
Skurhse Rage 🌆🌃🌌 <skurhse@rage.codes>
2023-03-31 21:33:30 -0500

    mongodb.bash

--- apt/mongodb.bash
+++ apt/mongodb.bash
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-# REQ: Installs MongoDB with APT. <skr 2023-03-27>
+# REQ: Installs the MongoDB APT repository and signing key. <skr>
 
 # SEE: https://www.mongodb.com/docs/manual/tutorial/install-mongodb-on-debian/ <>
+
+# PORT: Only x86_64 "Bullseye" is currently supported. <>
+
+# SEE: https://repo.mongodb.org/apt/debian/dists/ <2023-03-31>
 
 set +o braceexpand
 set -o errexit
@@ -12,43 +16,36 @@ set -o nounset
 set -o pipefail
 set -o xtrace
 
-# PORT: Only x86_64 "Bullseye" is currently supported. <2023-03-27>
-release='bullseye' # $(lsb_release -cs)
-architecture='amd64' # $(dpkg --print-architecture)
+readonly list='/etc/apt/sources.list.d/mongodb.list'
+readonly origin='mongodb'
 
-package='mongodb-org'
-version='6.0'
+readonly archive_type='deb'
+readonly architecture='amd64'
+readonly signed_by='/usr/share/keyrings/mongodb-server-6.0.gpg'
+readonly repository_url='https://repo.mongodb.org/apt/debian'
+readonly distribution='bullseye/mongodb-org/6.0'
+readonly component='main'
 
-keyserver='https://pgp.mongodb.com/server-6.0.asc'
-keyring='/usr/share/keyrings/mongodb-server-6.0.gpg'
-fingerprint='0x6A26B1AE64C3C388'
+readonly keyserver='https://pgp.mongodb.com/server-6.0.asc'
+readonly fingerprint='39BD841E4BE5FB195A65400E6A26B1AE64C3C388'
 
-archive_type='deb'
-repository_url='https://repo.mongodb.org/apt/debian'
-architecture=$(dpkg --print-architecture)
-distribution="$(lsb_release -cs)/$package/$version"
-component='main'
-list='/etc/apt/sources.list.d/dotnet.list'
+gpg --show-keys <(curl -L "$keyserver")
 
+sudo gpg \
+   --no-default-keyring                \
+	 --keyring            "$signed_by"   \
+	 --keyserver          "$keyserver"   \
+   --recv-keys          "$fingerprint"
 
-gpg --show-keys --keyid-format 0xLONG <(curl -L $keyserver)
-
-sudo gpg --no-default-keyring               \
-	 --keyring             $keyring     \
-	 --keyserver           $keyserver   \
-         --recv-keys           $fingerprint
-
-sudo gpg --no-default-keyring          \
-	 --keyring            $keyring \
-	 --keyid-format       0xLONG   \
+sudo gpg \
+   --no-default-keyring              \
+   --keyring            "$signed_by" \
 	 --list-keys
 
-str="$archive_type [arch=$architecture signed-by=$keyring] $repository_url $distribution $component"
+source="${archive_type} [arch=${architecture} signed-by=${signed_by}] ${repository_url} ${distribution} ${component}"
 
-sudo bash -c "echo ${str@Q} > $list"
-cat $list
+sudo bash -c "echo ${source@Q} > $list"
+cat "$list"
 
 sudo apt-get update
-sudo apt-get --assume-yes install $package
-
-mongosh --version
+sudo aptitude search "?origin(${origin})"
