732798fdd4a5f1afbb30e7731363f32a8874fac1
Skurhse Rage 🌆🌃🌌 <skurhse@rage.codes>
2023-04-03 07:34:15 -0500

    Update docker custom

--- custom/docker.bash
+++ custom/docker.bash
@@ -1,31 +1,32 @@
 #!/usr/bin/env bash
 
-# REQ: Installs and configures Docker. <skr 2023-03-31
+# REQ: Installs Docker ands configures the current shell. <skr 2023-03-31>
 
 # SEE: https://docs.docker.com/engine/install/debian/#install-using-the-repository <>
 # SEE: https://docs.docker.com/engine/install/linux-postinstall/ <>
 
-set +o braceexpand
-set -o errexit
-set -o noclobber
-set -o noglob
-set -o nounset
-set -o pipefail
+# NOTE: This script is intended to be sourced. <>
+
 set -o xtrace
 
-readonly packages=(
-  'docker-ce'
-  'docker-ce-cli'
-  'containerd.io'
-  'docker-buildx-plugin'
-  'docker-compose-plugin'
-)
+function main()
+{
+  set -o errexit
 
-sudo apt-get update
-sudo apt-get install "${packages[@]}"
+  readonly packages=(
+    docker-ce
+    docker-ce-cli
+    containerd.io
+    docker-buildx-plugin
+    docker-compose-plugin
+  )
 
-sudo groupadd -f docker
-sudo usermod -aG docker $USER
-newgrp docker <<'EOF'
-docker run hello-world
-EOF
+  # CAVEAT: Group changes must precede service installation. <skr>
+  sudo groupadd -f docker
+  sudo usermod -aG docker $USER
+
+  sudo apt-get update
+  sudo apt-get install --yes ${packages[@]}
+}
+
+main && exec sudo --preserve-env --user $USER bash -c 'docker run hello-world;exec bash'
