d5f789d5d87993a5b46e57cf51b7f6ab537c9651
Skurhse Rage 🌆🌃🌌 <skurhse@rage.codes>
2023-04-05 13:56:38 -0500

    Update nodejs

--- apt/nodejs.bash
+++ apt/nodejs.bash
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-# REQ: Installs nodejs using apt. <skr 2023-02-11> 
+# REQ: Installs the NodeSource APT repository and signing key. <skr 2023-04-05> 
 
-# SEE: https://github.com/nodesource/distributions/blob/master/README.md#installation-instructions <>
+# SEE: https://nodejs.org/en/download/package-manager#debian-and-ubuntu-based-linux-distributions <>
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 set +o braceexpand
 set -o errexit
@@ -12,34 +16,37 @@ set -o nounset
 set -o pipefail
 set -o xtrace
 
-keyserver='https://deb.nodesource.com/gpgkey/nodesource.gpg.key'
-keyring='/usr/share/keyrings/nodesource.gpg'
-fingerprint='0x1655A0AB68576280'
+source /etc/os-release
 
-repo='https://deb.nodesource.com/node_18.x'
-arch='amd64'
-list='/etc/apt/sources.list.d/nodesource.list'
-package='nodejs'
+declare -A key
+key[server]=https://deb.nodesource.com/gpgkey/nodesource.gpg.key
+key[ring]=/usr/share/keyrings/nodesource.gpg
+key[fingerprint]=9FD3B784BC1C6FC31A8A0A1C1655A0AB68576280
+
+declare -A src
+src[archive_type]=deb
+src[architecture]=$(dpkg --print-architecture)
+src[signed-by]=${key[ring]}
+src[repository_url]=https://deb.nodesource.com/node_18.x
+src[distribution]=$VERSION_CODENAME
+src[component]=main
+
+readonly list=/etc/apt/sources.list.d/nodesource.list
+
+gpg --version
+
+sudo gpg \
+  --no-default-keyring                     \
+  --keyring            ${key[ring]}        \
+  --keyserver          ${key[server]}      \
+  --recv-keys          ${key[fingerprint]}
+
+entry=${src[archive_type]}
+entry+=" [arch=${src[architecture]} signed-by=${src[signed-by]}]"
+entry+=" ${src[repository_url]}"
+entry+=" ${src[distribution]}"
+entry+=" ${src[component]}"
+
+sudo bash -c "echo ${entry@Q} > ${list@Q}"
 
 sudo apt-get update
-sudo apt-get install --assume-yes curl gpg lsb-release
-
-gpg --show-keys --keyid-format 0xLONG <(curl $keyserver)
-
-sudo gpg --no-default-keyring               \
-	 --keyring             $keyring     \
-	 --keyserver           $keyserver   \
-         --recv-keys           $fingerprint
-
-sudo gpg --no-default-keyring          \
-	 --keyring            $keyring \
-	 --keyid-format       0xLONG   \
-	 --list-keys
-
-str="deb [arch=$arch signed-by=$keyring] $repo $(lsb_release -cs) main"
-
-sudo bash -c "echo ${str@Q} > $list"
-cat "$list"
-
-sudo apt-get update
-sudo apt-get --assume-yes install "$package"
