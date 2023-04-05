730559162c6d99b4293a32460da63069ac789fe8
Skurhse Rage 🌆🌃🌌 <skurhse@rage.codes>
2023-04-05 00:01:10 -0500

    enbetter

--- custom/docker.bash
+++ custom/docker.bash
@@ -11,36 +11,13 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+set +o braceexpand
 set -o errexit
-
-readonly exec=(exec sudo --preserve-env --user $USER bash)
-
-function handle_int()
-{
-  echo 'Script Interrupted.'
-
-  if [[ $SHLVL -eq 1 ]]; then
-    ${exec[@]}
-  fi
-}
-trap handle_int INT
-
-function handle_exit()
-{
-  if [[ $? -ne 0 ]]; then
-    echo 'Something went terribly wrong.'
-  else
-    if [[ $SHLVL -ne 1 ]]; then
-      echo "WARN: Script SHLVL is $SHLVL."
-      echo "To complete installation, please refresh your top-level shell process:"
-      echo "  ${exec[@]}"
-      exit
-    fi
-  fi
-   
-  ${exec[@]}
-}
-trap handle_exit EXIT
+set -o noclobber
+set -o noglob
+set -o nounset
+set -o pipefail
+set -o xtrace
 
 readonly packages=(
   containerd.io
@@ -50,10 +27,43 @@ readonly packages=(
   docker-compose-plugin
 )
 
-# CAVEAT: Group changes must precede service installation. <skr>
-sudo groupadd --force docker
-sudo usermod --append --groups docker $USER
+readonly exec=(exec sudo --preserve-env --user $USER -- bash)
 
-sudo apt-get update
-sudo apt-get install --yes ${packages[@]}
-sudo --user $USER bash -c 'docker run hello-world'
+function handle_interrupt()
+{
+  echo 'ZIM: I was rudely interrupted.'
+
+  if [[ $SHLVL -eq 1 ]]; then
+    ${exec[@]}
+  fi
+}
+trap handle_interrupt INT
+
+function handle_exit()
+{
+  set +o xtrace
+
+  if [[ $? -ne 0 ]]; then
+    echo 'ZIM: I am afraid something went terribly wrong.'
+    exit 1
+  fi
+
+  if [[ $SHLVL -ne 1 ]]; then
+    echo "ZIM: Script SHLVL is $SHLVL."
+    echo "To complete installation, please refresh your top-level shell process:"
+    echo "  ${exec[@]}"
+    exit 0
+  fi
+
+  echo 'ZIM: Bravo! You have successfully installed Docker.'
+  ${exec[@]}
+}
+trap handle_exit EXIT
+
+# CAVEAT: Group changes must precede service installation. <skr>
+sudo groupadd --force -- docker
+sudo usermod --append --groups docker -- $USER
+
+sudo apt-get update --
+sudo apt-get install --yes -- ${packages[@]}
+sudo --user $USER -- bash -c 'docker run hello-world'
