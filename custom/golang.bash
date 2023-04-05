84eb8d66e6e9b286bbe186bb3b1e00ef47d68420
Skurhse Rage 🌆🌃🌌 <skurhse@rage.codes>
2023-04-05 11:47:13 -0500

    Update golang

--- custom/golang.bash
+++ custom/golang.bash
@@ -1,21 +1,50 @@
 #!/usr/bin/env bash
 
-# REQ: Installs go from the official release archive. <skr 2021-01-16>
-# SEE: https://go.dev/doc/install <>
-# ..............................................................................
-set +B -Cefuxvo pipefail
+# REQ: Installs a go binary release. <skr 2023-04-05>
 
-version=1.17.6
-archive=go$version.linux-amd64.tar.gz
+# SEE: https://go.dev/doc/install <>
+# SEE: https://go.dev/dl/ <>
+
+# TODO: Setup for exec and SHLVL. <>
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+set +o braceexpand
+set -o errexit
+set -o noclobber
+set -o noglob
+set -o nounset
+set -o pipefail
+set -o xtrace
+
+declare -A release
+release[version]='1.20.3'
+release[architecture]=$(dpkg --print-architecture)
+release[archive]="go${release[version]}.linux-${release[architecture]}.tar.gz"
+release[url]="https://golang.org/dl/${release[archive]}"
+
+declare -A install
+install[path]='/usr/local/go'
+install[dir]=$(dirname ${install[path]})
+install[export]='export PATH="$PATH:"'${install[path]@Q}'/bin'
+install[profile]=~/.bash_profile
+
+export=(export 'PATH="$PATH:"'${install[path]@Q}'/bin')
 
 cd /tmp
+wget --version
+wget --timestamping -- ${release[url]}
 
-wget --timestamping https://go.dev/dl/$archive
+sudo rm --recursive --force -- ${install[path]}
+sudo tar --directory ${install[dir]} --extract -gunzip --file ${release[archive]}
 
-sudo rm --recursive --force /usr/local/go/
+grep --version
+if ! grep --quiet --line-regexp --fixed-strings -- "${export[*]}" ${install[profile]}
+then
+  printf "\n${export[*]}\n" >> ${install[profile]}
+fi
 
-sudo tar --directory /usr/local --extract --gunzip --file $archive
-
-export PATH="$PATH:/usr/local/go/bin"
-
+eval ${export[@]}
 go version
