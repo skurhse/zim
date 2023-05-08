03ffb56ed2456a6d8411fe75d330dafc8a7082f3
Skurhse Rage 🌆🌃🌌 <skurhse@rage.codes>
2023-05-08 05:47:33 -0500

    update gopls

--- utils/neovim/language-servers/gopls.bash
+++ utils/neovim/language-servers/gopls.bash
@@ -2,4 +2,18 @@
 
 # REQ: Installs gopls with go get. <skr 2023-04-05>
 
-sudo GOBIN=/usr/local/go/bin go install golang.org/x/tools/gopls@latest
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+set +o braceexpand
+set -o errexit
+set -o noclobber
+set -o noglob
+set -o nounset
+set -o pipefail
+set -o xtrace
+
+readonly gobin="/usr/local/go/bin"
+
+sudo GOBIN="$gobin" "$gobin/go" install golang.org/x/tools/gopls@latest
