c7629dcb43fba44e7fef01b27b1538b8d8263747
Skurhse Rage 🌆🌃🌌 <skurhse@rage.codes>
2023-05-30 21:25:01 -0500

    fix kubic

--- apt/kubic/add_src.bash
+++ apt/kubic/add_src.bash
@@ -3,9 +3,6 @@
 # REQ: Installs the kubic apt repository and signing key. <eris 2023-05-25>
 # SEE: https://podman.io/docs/installation#debian <>
 
-# !!!: Not presently working. <eris 2023-05-26>
-# SEE: https://github.com/containers/podman/issues/18704 <>
-
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
@@ -20,7 +17,7 @@ set -o xtrace
 
 # NOTE: Key must be dearmored and imported. <eris 2023-05-25>
 declare -A key
-key[download]=https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/Debian_Testing/Release.key
+key[download]=https://download.opensuse.org/repositories/devel:kubic:libcontainers:unstable/Debian_Testing/Release.key
 key[ring]=/usr/share/keyrings/kubic.gpg
 key[fingerprint]=2472D6D0D2F66AF87ABA8DA34D64390375060AA4
 readonly key
@@ -29,7 +26,7 @@ declare -A src
 src[archive_type]=deb
 src[architecture]=$(dpkg --print-architecture)
 src[signed-by]=${key[ring]}
-src[repository_url]=http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/Debian_Testing/
+src[repository_url]=http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/unstable/Debian_Testing/
 src[distribution]=/
 #src[component]=main
 readonly src
