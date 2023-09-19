d59ec6e3ac5420fd7955ebafa117f548d8e1d14e
Skurhse Rage 🌆🌃🌌 <skurhse@rage.codes>
2023-09-19 18:44:23 -0500

    Update golang

--- custom/golang.bash
+++ custom/golang.bash
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
-# REQ: Installs a go binary release. <skr 2023-04-05>
+# REQ: Installs a go binary release. <rabbit 2023-09-19>
 
 # SEE: https://go.dev/doc/install <>
-# SEE: https://go.dev/dl/ <>
-
-# TODO: Setup for exec and SHLVL. <>
 
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -19,31 +16,33 @@ set -o nounset
 set -o pipefail
 set -o xtrace
 
-declare -A release
-release[version]='1.20.3'
-release[architecture]=$(dpkg --print-architecture)
-release[archive]="go${release[version]}.linux-${release[architecture]}.tar.gz"
-release[url]="https://golang.org/dl/${release[archive]}"
+arch=$(dpkg --print-architecture); readonly arch
 
-declare -A install
-install[path]='/usr/local/go'
-install[dir]=$(dirname ${install[path]})
-install[export]='export PATH="$PATH:"'${install[path]@Q}'/bin'
-install[profile]=~/.bash_profile
+readonly version='1.21.1'
+readonly checksum='b3075ae1ce5dab85f89bc7905d1632de23ca196bd8336afd93fa97434cfa55ae'
 
-export=(export 'PATH="$PATH:"'${install[path]@Q}'/bin')
+readonly archive="go$version.linux-$arch.tar.gz"
+readonly url="https://golang.org/dl/$archive"
+
+readonly path='/usr/local/go'
+readonly profile=~/'.bash_profile'
+
+readonly export=(export "PATH=\"\$PATH:\"${path@Q}/bin")
+
+dir=$(dirname "$path"); readonly dir
 
 cd /tmp
 wget --version
-wget --timestamping -- ${release[url]}
+wget --timestamping "$url"
 
-sudo rm --recursive --force -- ${install[path]}
-sudo tar --directory ${install[dir]} --extract -gunzip --file ${release[archive]}
+sha256sum --check <<<"$checksum $archive"
 
-grep --version
-if ! grep --quiet --line-regexp --fixed-strings -- "${export[*]}" ${install[profile]}
+sudo rm --recursive --force "$path"
+sudo tar --extract --gunzip --directory "$dir" --file "$archive"
+
+if ! grep --quiet --line-regexp --fixed-strings -- "${export[*]}" "$profile"
 then
-  printf "\n${export[*]}\n" >> ${install[profile]}
+  printf "\n${export[*]}\n" >> "$profile"
 fi
 
 eval ${export[@]}
