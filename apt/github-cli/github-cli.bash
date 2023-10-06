2512202e222d784db14ee9349cb481bf87a86645
Skurhse Rage 🌆🌃🌌 <skurhse@rage.codes>
2023-10-05 19:53:08 -0500

    Update

--- apt/github-cli/github-cli.bash
+++ apt/github-cli/github-cli.bash
@@ -1,42 +1,42 @@
 #!/usr/bin/env bash
 
-# REQ: Installs the GitHub CLI via APT. <skr 2023-01-21>
+# REQ: Installs the GitHub CLI. <rbt 2023-10-05>
+
+# SEE: https://github.com/cli/cli/blob/trunk/docs/install_linux.md <> 
 
 set +o braceexpand
-
 set -o errexit
 set -o noclobber
 set -o noglob
 set -o nounset
 set -o xtrace
 
+readonly keyserver='https://cli.github.com/packages/githubcli-archive-keyring.gpg'
+readonly keyring='/usr/share/keyrings/githubcli-archive-keyring.gpg'
+readonly fingerprint='2C6106201985B60E6C7AC87323F3D4EA75716059'
+
+arch="$(dpkg --print-architecture)"
+readonly component='main'
+readonly distro='stable'
+readonly file='/etc/apt/sources.list.d/github-cli.list'
+readonly url='https://cli.github.com/packages'
+
+readonly entry="deb [arch=$arch signed-by=$keyring] $url $distro $component"
+
+readonly extensions=(
+  'https://github.com/nektos/gh-act'
+)
+
 realpath=$(realpath "${BASH_SOURCE[0]}")
 dirname=$(dirname "$realpath")
 cd "$dirname"
 
-source ../../lib/apt.bash
+sudo gpg --no-default-keyring \
+  --keyring "$keyring" --keyserver "$keyserver" --recv-keys "$fingerprint"
 
-function main() {
-  url='https://cli.github.com/packages/githubcli-archive-keyring.gpg'
-  file='/usr/share/keyrings/githubcli-archive-keyring.gpg'
-  make_keyring
+sudo bash -c "echo ${entry@Q} > ${file@Q}"
 
-  arch="$(dpkg --print-architecture)"
-  component='main'
-  distribution='stable'
-  file='/etc/apt/sources.list.d/github-cli.list'
-  signed_by="${keyring[file]}"
-  url='https://cli.github.com/packages'
-  make_list
+sudo apt-get update
+sudo apt-get install --assume-yes -- gh
 
-  download_keyring
-  install_list
-
-  update_lists
-  install_packages gh
-
-  gh version
-}
-
-main "$@"
-
+gh extension install --force "${extensions[@]}"
