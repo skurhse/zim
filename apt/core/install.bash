c3307ea187b2e78df6460db809a231b6c202ec1d
Skurhse Rage 🌆🌃🌌 <skurhse@rage.codes>
2023-10-09 20:11:03 -0500

    note

--- apt/core/install.bash
+++ apt/core/install.bash
@@ -4,6 +4,8 @@
 
 # SEE: https://packages.debian.org/bookworm/ <>
 
+# TODO: Get rid of this file. <rbt 2023-10-09>
+
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
