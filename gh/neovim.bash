31ddf8b3de1db4877446bde75814e5a0699e2343
rabbit <skurhse@rage.codes>
2024-10-21 18:19:33 -0500

    nvim

--- gh/neovim.bash
+++ gh/neovim.bash
@@ -2,22 +2,32 @@
 
 # installs neovim stable. <skr 2023-01-25>
 
+# see: https://github.com/neovim/neovim/blob/master/INSTALL.md#linux <rbt 2024-10-01>
+
 set +B -Cefuxo pipefail
 
 repo=neovim/neovim
-dir=/tmp/$repo
 tag=stable
+name=nvim-linux64
 
-rm -rf $dir; mkdir -p $dir
+cd /tmp
 
-gh release download $tag    \
-  --repo    $repo           \
-  --dir     $dir            \
-  --pattern *.deb           \
-  --pattern *.deb.sha256sum
+gh release download --clobber $tag --repo $repo --dir /tmp --pattern "$name*"
 
-set +o noglob
+sha256sum --check $name.tar.gz.sha256sum
 
-cd $dir && sha256sum --check *.deb.sha256sum
+sudo rm -rf /opt/$name
 
-sudo apt-get install --assume-yes $dir/*.deb
+sudo tar -C /opt -xzf $name.tar.gz
+
+export=(export "PATH=\"\$PATH\":/opt/$name/bin")
+profile=~/.bash_profile
+
+if ! grep --quiet --line-regexp --fixed-strings -- "${export[*]}" "$profile"
+then
+  printf "\n${export[*]}\n" >> "$profile"
+fi
+
+eval ${export[@]}
+
+nvim -V1 --version
