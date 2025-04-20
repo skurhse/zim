c4bfa04150adb3da6cfe409c962df215dbef05f5
rabbit <skurhse@rage.codes>
2025-04-20 15:03:46 -0500

    Update jq

--- gh/jq.bash
+++ gh/jq.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# REQ: Installs the latest jq from Github Releases. <skr 2023-03-16>
+# REQ: Installs the latest jq from Github Releases. <rbt 2025-04-20>
 
 # SEE: https://github.com/stedolan/jq <>
 
@@ -17,22 +17,20 @@ shopt -s extglob
 readonly repo="stedolan/jq"
 readonly pattern="*.tar.gz"
 
-autoreconf --version
 gh --version
 make --version
 
 cd /tmp
 rm -rf jq/
 mkdir jq
+
 cd jq
 gh release download --clobber --repo "$repo" --pattern "$pattern"
 tar -xzf $pattern
+
 cd !($pattern)
-# HACK: Autoreconf is required to resolve Oniguruma make target. <>
-# SEE: https://github.com/stedolan/jq/issues/2172#issuecomment-674547200 <>
-autoreconf -fi
 ./configure
 make
 sudo make install
 
-
+jq --version
