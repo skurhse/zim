d944f1a0441b934b1b7569a1740a2a4b3b2116f3
Skurhse Rage ∅ <skurhse@rage.codes>
2025-04-28 20:45:06 -0500

    Update install.bash

--- gh/ruby-install/install.bash
+++ gh/ruby-install/install.bash
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 # REQ: Installs ruby-install. <rbt 2025-04-28>
 
 # SEE: https://github.com/postmodern/ruby-install#install <>
